49c96a95c2af68426e2243926c7af01f3e7d3773
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-07-09 20:48:15 +0700

    v-user

--- apps/v-user/src/main.ts
+++ apps/v-user/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AuthModule } from './auth.module';
+import { AuthModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);

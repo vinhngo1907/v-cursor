e446837a983db49dad9fa6d69e0c136625241fda
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-07-20 17:11:15 +0700

    app v-messages

--- apps/v-message/src/app.module.ts
+++ apps/v-message/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MessagesModule } from './modules/messages/messages.module';
+import { MessagesRepoModule } from './modules/messages-repo/messages-repo.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: [
+                // __dirname + '/../config/.env.prod',
+                __dirname + '/../config/.env.dev',
+            ],
+            isGlobal: true,
+        }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => {
+                return { uri: configService.get<string>('MONGO_URI') }
+            },
+        }),
+        MessagesModule,
+        MessagesRepoModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }

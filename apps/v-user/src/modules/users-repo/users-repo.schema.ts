f31288e62e62b499dc5a8c8780930425c23757be
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-07-20 10:59:33 +0700

    commit

---
+++ apps/v-user/src/modules/users-repo/users-repo.schema.ts
@@ -0,0 +1,31 @@
+import { HydratedDocument } from "mongoose";
+import { userAnalysisDto } from 'micro-dto';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ type: String, required: true, unique: true })
+  login: string;
+  
+  @Prop({ type: String; required: true, unique: true })
+  email: string;
+  
+  @Prop({ type: String, required: true })
+  password: string;
+
+  @Prop({ type: String, required: true })
+  salt: string;
+
+  @Prop({ type: Boolean })
+  active: boolean;
+
+  @Prop({ type: Date })
+  created_at: Date;
+
+  @Prop({ type: Object })
+  analysis: userAnalysisDto;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);

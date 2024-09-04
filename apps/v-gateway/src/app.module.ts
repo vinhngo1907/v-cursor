4a9c94d3feab3946e67f7b8fe8ccef04a74a729a
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-09-04 19:17:09 +0700

    chu ba bi bo nha nho

--- apps/v-gateway/src/app.module.ts
+++ apps/v-gateway/src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbConfig } from './config/db';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dbConfig), UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [TypeOrmModule.forRoot(dbConfig), UsersModule, AuthModule],
+	controllers: [],
+	providers: [],
 })
-export class AppModule {}
+export class AppModule { }

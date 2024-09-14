6e59e8f0c1036f846f10c9f19a1877fa251a1add
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-09-14 10:55:44 +0700

    add and change something

--- apps/v-gateway/src/modules/messages/messages.module.ts
+++ apps/v-gateway/src/modules/messages/messages.module.ts
@@ -1,12 +1,11 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
 
 @Module({
-    imports: [HttpModule],
-    controllers: [MessagesController],
-      providers: [MessagesService],
-      exports: [MessagesService]
+	imports: [HttpModule],
+	controllers: [],
+	providers: [MessagesService],
+	exports: [MessagesService]
 })
 export class MessagesModule { }

f2f9499cbceb55c59b188192303d65dcdacfe5fd
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-09-14 10:44:31 +0700

    something nice

--- apps/v-gateway/src/modules/auth/auth.service.ts
+++ apps/v-gateway/src/modules/auth/auth.service.ts
@@ -1,9 +1,9 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AuthRepo } from './auth.repo';
 import { JwtService } from '@nestjs/jwt';
-import { WebUserDto } from '@libs/v-dto';
+import { WebLoginParamDto, WebRegistrationParamDto, WebUserDto } from '@libs/v-dto';
 import { WebAccessTokens } from './auth.dto';
 
 @Injectable()
@@ -15,5 +15,53 @@ export class AuthService {
     ) { }
     errorMessage: string = 'Oops something went wrong';
 
-    private async getTokens(user:WebUserDto) :Promise<WebAccessTokens>{}
+    private async getTokens(user: WebUserDto): Promise<WebAccessTokens> {
+        const { login, id, email, created_at, active } = user;
+
+        const [accessToken, refreshToken] = await Promise.all([
+            this.jwtService.signAsync(
+                { id, login, email, active, created_at },
+                {
+                    secret: this.configService.get<string>('JWT_SECRET'),
+                    expiresIn: this.configService.get<string>('JWT_EXPIRES'),
+                }
+            ),
+            this.jwtService.signAsync(
+                { id, login, email, active, created_at },
+                {
+                    secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+                    expiresIn: this.configService.get<string>('JWT_REFRESH_EXPIRES'),
+                },
+            )
+        ]);
+
+        return { accessToken, refreshToken };
+    }
+
+    async registration(registrationDto: WebRegistrationParamDto): Promise<WebUserDto> {
+        const { id, email, created_at, login, active } = await this.authRepo.registration(registrationDto);
+        return { id, email, created_at, login, active };
+    }
+
+    async login(loginDto: WebLoginParamDto): Promise<WebAccessTokens> {
+        const { active, created_at, email, id, login } = await this.authRepo.login(loginDto);
+        const { accessToken, refreshToken } = await this.getTokens({ active, created_at, email, id, login });
+        return { accessToken, refreshToken };
+    }
+
+    async refresh(user: WebUserDto): Promise<WebAccessTokens> {
+        if (!user.id) {
+            throw new UnauthorizedException();
+        }
+
+        const activeUser = await this.authRepo.findUserById({ id: user.id });
+        if (!activeUser) {
+            throw new UnauthorizedException();
+        }
+
+        const { id, login, created_at, email, active } = activeUser;
+        const { accessToken, refreshToken } = await this.getTokens({ id, login, created_at, email, active });
+        
+        return { accessToken, refreshToken };
+    }
 }

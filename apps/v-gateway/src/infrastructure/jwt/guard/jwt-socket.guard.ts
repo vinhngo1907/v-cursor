d8beb7a2afb68ac7c8fdef0a4d98c6819ef2aeb8
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-09-14 10:51:37 +0700

    commit

---
+++ apps/v-gateway/src/infrastructure/jwt/guard/jwt-socket.guard.ts
@@ -0,0 +1,25 @@
+import { ConfigService } from '@nestjs/config';
+import { WsException } from '@nestjs/websockets';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import * as jwt from 'jsonwebtoken';
+
+@Injectable()
+export class JwtSocketGuard implements CanActivate {
+    constructor(private configService: ConfigService) { }
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        try {
+            const client = context.switchToWs().getClient();
+            const data = context.switchToWs().getData();
+            const user: any = jwt.verify(
+                data.token || '',
+                this.configService.get<string>('JWT_SECRET'),
+            );
+            client.handshake.user = user;
+            return true;
+        } catch (error) {
+            throw new WsException(error.message);
+        }
+    }
+}

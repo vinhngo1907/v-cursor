6d9f8b660ed65ecdcae66d805d1b0261ec67dfcf
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-09-14 08:39:37 +0700

    v-client

--- apps/v-client/src/components/HeaderAlert.vue
+++ apps/v-client/src/components/HeaderAlert.vue
@@ -6,15 +6,23 @@ defineProps<{ msg: string }>()
 const count = ref(0)
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
+export default {
+  data: function(){
+    const headerAlertStore = useHeaderAlertStore();
+    return {
+      headerAlertStore: headerAlertStore,
+    }
+  }
+}
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+<template>
+  <v-alert
+    v-if="Boolean(headerAlertStore.success)"
+    icon="mdi-alert-circle-outline"
+    variant="outlined"
+    color="success"
+    title="Success"
+  >
   </div>
 
   <p>
